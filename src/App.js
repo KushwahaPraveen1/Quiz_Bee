374cc1e5e5529ce3b7dcb6a813bf60e968f3b3e4
KushwahaPraveen1 <kushwahapraveen0507@gmail.com>
2023-01-03 14:46:33 +0530

    Added Component folder and Files related of components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route,Switch } from 'react-router-dom'
-import Home from './component/Home';
-import Quiz from './component/Quiz'
+// import Home from './component/Home';
+// import Quiz from './component/Quiz'
 import './App.css'
 
 
