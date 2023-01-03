959743f76bf20d976ea7f5c0d9d6a77c0edd9601
KushwahaPraveen1 <kushwahapraveen0507@gmail.com>
2023-01-03 17:52:14 +0530

    Added Questions

---
+++ src/component/QuizData.js
@@ -0,0 +1,56 @@
+export const QuizData=[
+    {
+        question:"What is ReactJS ?",
+        answer:[
+            {ans:"Server-Side Framework ",isCorrect:false},
+            {ans:"User-Interface Framework",isCorrect:true},
+            {ans:"Both A and B",isCorrect:false},
+            {ans:"None of the Above",isCorrect:false}
+        ]
+    },
+    {
+        question:"What of the following is used in React.js to increase performance ?",
+        answer:[
+            {ans:"Original DOM ",isCorrect:false},
+            {ans:"Virtual DOM",isCorrect:true},
+            {ans:"Both A and B",isCorrect:false},
+            {ans:"None of the above",isCorrect:false}
+        ]
+    },
+    {
+        question:"How many numbers of elements a valid react component can return ?",
+        answer:[
+            {ans:"1",isCorrect:true},
+            {ans:"2",isCorrect:false},
+            {ans:"4",isCorrect:false},
+            {ans:"5",isCorrect:false}
+        ]
+    },
+    {
+        question:"What is a state in React?",
+        answer:[
+            {ans:"A permanent storage. ",isCorrect:false},
+            {ans:"Internal storage of the component.",isCorrect:true},
+            {ans:"External storage of the component.",isCorrect:false},
+            {ans:"None of the above.",isCorrect:false}
+        ]
+    },
+    {
+        question:" Which of the following method refers to the parent class in React.js ?",
+        answer:[
+            {ans:"inherits()",isCorrect:false},
+            {ans:"self()",isCorrect:false},
+            {ans:"super()",isCorrect:true},
+            {ans:"this()",isCorrect:false}
+        ]
+    },
+    {
+        question:"Which of the following method is not a part of ReactDOM ?",
+        answer:[
+            {ans:"ReactDOM.destroy()",isCorrect:true},
+            {ans:"ReactDOM.hydrate()",isCorrect:false},
+            {ans:"ReactDOM.createPortal()",isCorrect:false},
+            {ans:"ReactDOM.findDOMNode()",isCorrect:false}
+        ]
+    }
+]
